d6ceecd6622f1a3c4ac003443083d4c98927969f
Florian Baumard <florianbaumard4@gmail.com>
2024-12-03 11:52:41 -0500

    Correction après revue

--- app/src/main/java/com/example/samride/services/RegisterService.kt
+++
@@ -1,7 +0,0 @@
-package com.example.samride.services
-
-class RegisterService {
-    fun isValidEmail(email: String): Boolean {
-        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
-    }
-}
